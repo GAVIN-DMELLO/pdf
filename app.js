f88521b891ea0165043643773521f75bd6042d4a
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-06-28 23:57:03 +0530

    feat: add server.js and app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import pdfRoutes from "./routes/pdf.routes.js";
+
+const app = express();
+
+//deserialiser
+app.use(express.json());
+
+// subroute
+app.use("/api/v1/pdf", pdfRoutes);
+
+//error handler for non matching route
+app.use("*", (req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
+export default app;
