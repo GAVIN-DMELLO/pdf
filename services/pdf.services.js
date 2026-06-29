1b73a52e00c0b60c25ff1e531eb5eb68cad4a579
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-06-29 17:47:14 +0530

    feat: add no of pages

--- services/pdf.services.js
+++ services/pdf.services.js
@@ -8,13 +8,9 @@ const client = new OpenAI({
 
 export const generateLatexCode = async (formData) => {
 
-    const { chapters, package:pkg } = formData;
+    const { chapters, package:pkg , pages } = formData;
 
-    const prompt = `Generate a LaTeX document using the report class. 
-    Use the lipsum package. Start the document and create ${chapters} chapters. 
-    Inside each chapter, include lipsum dummy text. 
-    End the document. 
-    Respond ONLY with raw LaTeX code. No markdown, no explanations do only as much as i said dont even try to add anything extra.`;
+    const prompt = `Generate latex code of ${pages}pages using report class , generate ${chapters}`;
 
     // Use chat.completions.create instead of responses.create
     const response = await client.chat.completions.create({
