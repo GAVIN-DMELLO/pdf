215028831fb6ab5fbf6c73a9b2f2b5031e04a99a
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-06-29 17:24:49 +0530

    feat: add all interfaces to the listening

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(` Server running on http://localhost:${PORT}`);
-  console.log(` Ready to handle requests at /api/v1/pdf/pdfcode`);
+app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
+  console.log(`Server running on port ${process.env.PORT || 5000}`);
 });
