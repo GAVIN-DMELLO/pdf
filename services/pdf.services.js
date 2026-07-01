4665a50bdf8cbafe4e225dd16dea15ddb34674e3
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-07-01 15:06:39 +0530

    feat: add the border on every page top and bottom

--- services/pdf.services.js
+++ services/pdf.services.js
@@ -1,64 +1,78 @@
 
 
 export const generateLatexCode = async (formData) => {
+    const { 
+        borderXShift, borderYShift, borderColor, hlength, thickness, 
+        verticalSpaceAfterBorder, verticalSpaceAfterHeading, 
+        verticalSpaceAfterSubheading, contentXShift, contentYShift, 
+        contentTopShift, contentBottomShift, heading, subHeading 
+    } = formData;
 
-    const { borderXShift , borderYShift , borderColor , hlength , thickness , verticalSpaceAfterBorder , verticalSpaceAfterHeading , verticalSpaceAfterSubheading , contentXShift , contentYShift , contentTopShift , contentBottomShift , heading , subHeading , } = formData;
-
-    // 1. Configuration/Template Block
-const documentHeader = `
+    // latex class and packages 
+    const latexPreamble = `
 \\documentclass{article}
 \\usepackage[a4paper,left=${contentXShift}cm,right=${contentYShift}cm,top=${contentTopShift}cm,bottom=${contentBottomShift}cm]{geometry}
 \\usepackage{tikz}
-\\begin{document}
-\\begin{tikzpicture}[remember picture,overlay]
-\\fill[fill=${borderColor}] ([xshift=${borderXShift}cm,yshift=-${borderYShift}cm]current page.north west) rectangle ++(${hlength}cm,-${thickness}cm);
-\\end{tikzpicture}
-`;
+\\usepackage{xcolor}
+\\usepackage{eso-pic}
 
-// 2. Content Block (The "Heading" area)
-const documentContent = `
-\\hspace{${verticalSpaceAfterBorder}cm}
-\\begin{center}
-\\textbf{\\LARGE ${heading}}\\\\[${verticalSpaceAfterHeading}cm]
-\\large ${subHeading}
-\\end{center}
-\\vspace{${verticalSpaceAfterSubheading}cm}
-`;
-
-// 3. Control Block (Breaks and Spacing)
-const paraBreak = `\\par\\vspace{1cm}`;
-
-// 4. Footer Block
-const documentFooter = `\\end{document}`;
-
-
-const paraArray = formData.para;
-const count = paraArray[0];
-
-console.log(count)
-
-let docParts = [documentHeader, documentContent];
-
-
-for (let i = 1; i <= count; i++) {
-    const paraText = paraArray[i];
+\\AddToShipoutPictureBG{
+  \\begin{tikzpicture}[remember picture,overlay]
+    % Top Border (Centered using current page.north anchor)
+    \\fill[fill=${borderColor}] ([xshift=-${hlength/2}cm, yshift=-${borderYShift}cm]current page.north) rectangle ++(${hlength}cm, -${thickness}cm);
     
-    // Add the text
-    docParts.push(paraText);
-    
-    // Add the break if it's not the last paragraph
-    if (i < count) {
-        docParts.push(paraBreak);
-    }
+    % Bottom Border (Centered using current page.south anchor)
+    \\fill[fill=${borderColor}] ([xshift=-${hlength/2}cm, yshift=${borderYShift}cm]current page.south) rectangle ++(${hlength}cm, ${thickness}cm);
+  \\end{tikzpicture}
 }
+`;
+
+    // 2. Document begin
+    const documentBegin = `\\begin{document}`;
 
 
-docParts.push(documentFooter);
+    
 
 
-const final = docParts.join("\n");
+    // 4. Heading and Subheading
+    const documentContent = 
+    `
+    \\hspace{${verticalSpaceAfterBorder}cm}
+    \\begin{center}
+    \\textbf{\\LARGE ${heading}}\\\\[${verticalSpaceAfterHeading}cm]
+    \\large ${subHeading}
+    \\end{center}
+    \\vspace{${verticalSpaceAfterSubheading}cm}
+    `;
 
 
+    // 5. Processing Paragraphs
+    const paraBreak = `\\par\\vspace{1cm}`;
+    const paraArray = formData.para;
+    const count = paraArray[0];
+    const paragraphs = [];
+
+    for (let i = 1; i <= count; i++) {
+        if (paraArray[i]) {
+            paragraphs.push(paraArray[i]);
+            // Add break only between paragraphs
+            if (i < count) {
+                paragraphs.push(paraBreak);
+            }
+        }
+    }
+
+    // 6. Document End
+    const documentFooter = `\\end{document}`;
+
+    // Assemble final document
+    const final = [
+        latexPreamble,
+        documentBegin,
+        documentContent,
+        ...paragraphs,
+        documentFooter
+    ].join("\n");
 
     return final;
 };
